b88253dce0042817469296b5618fc72d692464b7
863aditya <863aditya@gmail.com>
2024-12-27 18:59:36 +0530

    api upd

--- src/views/auth/api.js
+++ src/views/auth/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://13.232.86.90:3000/api',
   withCredentials: false,
 });
 
